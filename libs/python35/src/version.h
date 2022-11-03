4d81083b94f26d42625bee10940d7a1650a1e088
Alexander Guettler <alexander@guettler.io>
2022-11-03 11:13:06 -0700

    Bump version

--- libs/python35/src/version.h
+++ libs/python35/src/version.h
@@ -3,7 +3,7 @@
 // clang-format off
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               9
-#define VERSION_REVISION            1
+#define VERSION_REVISION            2
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
